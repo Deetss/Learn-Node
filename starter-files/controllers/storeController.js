feb3ccd0187bdcc95c70209e2b53c267a20ab48e
Deetss <dylan817@live.com>
2018-05-19 15:42:39 +0000

    Add store form

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,8 +1,18 @@
+const mongoose = require("mongoose");
+const Store = mongoose.model('Store');
+
+
 exports.homePage = (req, res) => {
     res.render('index');
 };
 
+exports.createStore = (req, res) => {
+    const store = new Store(req.body);
+    store.save();
+    res.redirect('/');
+}
+
 exports.addStore = (req, res) => {
-    res.send('It works');
+    res.render('editStore', { title: 'Add Store'});
 };
 
