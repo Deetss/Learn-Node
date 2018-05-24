e62f9928d0cca0827d683833a119568307b87153
Deetss <dylan817@live.com>
2018-05-24 04:08:33 +0000

    add stores

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -3,16 +3,23 @@ const Store = mongoose.model('Store');
 
 
 exports.homePage = (req, res) => {
-    res.render('index');
+    res.render('index', { title: 'Home'});
 };
 
-exports.createStore = (req, res) => {
-    const store = new Store(req.body);
-    store.save();
-    res.redirect('/');
-}
+exports.createStore = async (req, res) => {
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+    res.redirect(`/store/${store.slug}`);
+    
+};
 
 exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store'});
+};
+
+exports.getStores = async (req, res) => {
+    // 1. Query database for list of all stores
+    const stores = await Store.find();
+    res.render('stores', { title: 'Stores', stores});
 };
 
