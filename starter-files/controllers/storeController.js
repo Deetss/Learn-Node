916542d9ed12e10c054e81e2be7aa4d7d33b4867
deetss <dylan817@live.com>
2018-05-24 00:14:32 -0500

    Allow editing of stores, add attributes to stores

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -10,7 +10,7 @@ exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
-    
+
 };
 
 exports.addStore = (req, res) => {
@@ -22,4 +22,22 @@ exports.getStores = async (req, res) => {
     const stores = await Store.find();
     res.render('stores', { title: 'Stores', stores});
 };
+
+exports.editStore = async (req, res) => {
+    // 1. Find the store given the id
+    const store = await Store.findOne({ _id: req.params.id });
+    // 2. Comfirm they are the owner of the store
+    // TODO
+    // 3. Render out the edit form so the user can update their store
+    res.render('editStore', { title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+    const store = await Store.findOneAndUpdate({ _id: req.params.id}, req.body, { new: true, // Return the new store instead of the old one
+    runValidators: true
+    }).exec();
+    req.flash('success', `Successfully updated ${store.name}. <a href='/stores/${store.slug}'>View Store -></a>`);
+    res.redirect(`/stores/${store._id}/edit`)
+
+}
 
