9e8719b3e33cc66e0f7018dd5ebf6c5ba9a8e685
Deetss <dylan817@live.com>
2018-06-07 21:46:05 +0000

    add password reset flow, logic for authoring store edits

--- starter-files/controllers/authController.js
+++ starter-files/controllers/authController.js
@@ -1,4 +1,9 @@
 const passport = require('passport');
+const mongoose = require("mongoose");
+const User = mongoose.model('User');
+const crypto = require('crypto');
+const promisify = require("es6-promisify");
+const mail = require("../handlers/mail");
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -11,7 +16,7 @@ exports.logout = (req, res) => {
   req.logout();
   req.flash('success', 'You are now logged out!');
   res.redirect('/login');
-}
+};
 
 exports.isLoggedIn = (req,res,next) => {
   if(req.isAuthenticated()){
@@ -20,4 +25,78 @@ exports.isLoggedIn = (req,res,next) => {
   }
   req.flash('error', 'You must be logged in!');
   res.redirect('/login');
-}
+};
+
+exports.forgot = async (req, res) => {
+  // 1. See if a user with that email exists
+  const user  = await User.findOne({ email: req.body.email });
+  if(!user) {
+    req.flash('error', 'No account with that email exists!');
+    return res.redirect('back');
+  }
+  // 2. Set reset tokens and expiry on their account
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+  await user.save();
+  // 3. Send them an email with the token
+  const resetURL = `https://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+  await mail.send({
+    user,
+    subject: 'Password Reset',
+    resetURL,
+    filename: 'password-reset'
+  });
+  req.flash('success', `You have been emailed a password reset link.`);
+  // 4. redirect to login page
+  res.redirect('/login');
+};
+
+exports.reset = async (req, res) => {
+  const user = await User.findOne({ 
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  });
+  
+  if(!user){
+    req.flash('error', 'Password reset is invalid or has expired');
+    return res.redirect('/login');
+  }
+  
+  // if there is a user, show the password reset form
+  res.render('reset', {title: 'Reset your password'});
+};
+
+exports.confirmedPasswords = (req, res, next) => {
+  if (req.body.password === req.body['password-confirm']){
+    return next();
+  }
+  req.flash('error', 'Passwords do not match');
+  res.redirect('back');
+};
+
+exports.update = async (req, res) => {
+  const user = await User.findOne({ 
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  });
+  
+  if(!user){
+    req.flash('error', 'Password reset is invalid or has expired');
+    return res.redirect('/login');
+  }
+  
+  const setPassword = promisify(user.setPassword, user);
+  await setPassword(req.body.password);
+  
+  // nullify old token
+  user.resetPasswordExpires = undefined;
+  user.resetPasswordToken = undefined;
+  
+  const updatedUser = await user.save();
+  
+  // login after updating password
+  await req.login(updatedUser);
+  
+  req.flash('success', 'Nice! Your password has been reset! You are now logged in!');
+  res.redirect('/');
+};
--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -39,6 +39,7 @@ exports.resize = async (req, res, next) => {
 };
 
 exports.createStore = async (req, res) => {
+    req.body.author = req.user._id;
     const store = await (new Store(req.body)).save();
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
@@ -55,11 +56,17 @@ exports.getStores = async (req, res) => {
     res.render('stores', { title: 'Stores', stores});
 };
 
+const confirmOwner = (store, user) => {
+    if (!store.author.equals(user._id)){
+      throw Error('You must own a store in order to edit it');
+    }
+};
+
 exports.editStore = async (req, res) => {
     // 1. Find the store given the id
     const store = await Store.findOne({ _id: req.params.id });
     // 2. Comfirm they are the owner of the store
-    // TODO
+    confirmOwner(store, req.user);
     // 3. Render out the edit form so the user can update their store
     res.render('editStore', { title: `Edit ${store.name}`, store });
 };
@@ -75,7 +82,7 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
-  const store = await Store.findOne({slug: req.params.slug});
+  const store = await Store.findOne({slug: req.params.slug}).populate('author');
 
   if(!store) return next();
 
@@ -85,10 +92,10 @@ exports.getStoreBySlug = async (req, res, next) => {
 exports.getStoresByTag = async (req, res) => {
   const tag = req.params.tag;
   const tagsPromise = Store.getTagsList();
-  const tagQuery = tag || { $exists: true }
+  const tagQuery = tag || { $exists: true };
   const storesPromise = Store.find({tags: tagQuery });
   const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
 
   res.render('tag', {tags, title: 'Tags', tag, stores});
 
-}
+};
